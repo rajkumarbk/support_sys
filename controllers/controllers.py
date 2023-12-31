fcddbc22b43ad2cd9bb29360e31db58347381493
rajkumar <raj@rajkumars-MacBook-Air.local>
2023-12-31 13:05:40 +0545

    fixed ticket issue in frontend and backend

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -3,19 +3,69 @@ from odoo.http import request
 import base64
 
 class HomePageController(http.Controller):
-
     @http.route('/', type="http", auth='public', website=True)
-    def user_authentication_form(self, **kw):
-        return request.render('support_sys.homepage', {})
+    def user_authentication_form(self, **kwargs):
+            return request.render('support_sys.homepage', {})
     
-class ExpenseController(http.Controller):    
-    @http.route('/expense', type="http", auth='public', website=True)
-    def user_authentication_form(self, **kw):
-        return request.render('support_sys.expense', {})
+class EmployeeController(http.Controller): 
+    @http.route('/expense_history', type="http", auth='public', website=True)       
+    def list_expense(self, **kwargs):
+        expenses = request.env["employee.expense"].sudo().search([])
+
+        data = []
+        for expense in expenses:
+            photo = expense.photo.decode('utf-8') if expense.photo else None
+            data.append({
+                'nepalidatepicker':expense.nepalidatepicker,
+                'starting_location': expense.starting_location,
+                'ending_location': expense.ending_location,
+                'hotel_name':expense.hotel_name,
+                'hotel_address':expense.hotel_address,
+                'hotel_phone': expense.hotel_phone,
+                'expense_category': expense.expense_category,
+                'amount': expense.amount,
+                'description': expense.description,
+                'photo':photo,
+            })
+        return http.request.render(
+            "support_sys.expense_history",
+            {"records": data}
+        )
+    @http.route('/expense_form', type="http", auth='public', website=True)    
+    def authenticate_user(self, **post):
+        amount = post.get('amount')
+        description = post.get('description')
+        hotel_name=post.get('hotel_name')
+        hotel_address=post.get('hotel_address')
+        hotel_phone=post.get('hotel_phone')
+        expense_category= post.get('expense_category')
+        nepalidatepicker = post.get('nepalidatepicker')
+        starting_location=post.get('starting_location')
+        ending_location=post.get('ending_location')
+        photo=post.get('photo')
+
+        if photo:
+            photo_base64 = base64.b64encode(photo.read())
+        else:
+            photo_base64 = None
+
+        expense = request.env['employee.expense'].sudo().create({
+            'amount': amount,
+            'description': description,
+            'hotel_name':hotel_name,
+            'hotel_address':hotel_address,
+            'hotel_phone':hotel_phone,
+            'expense_category': expense_category,
+            'photo':photo_base64,
+            'nepalidatepicker': nepalidatepicker,
+            'starting_location': starting_location,
+            'ending_location':ending_location
+        })
+
+        return request.redirect('/expense_history')
     
 class TicketController(http.Controller):    
     @http.route('/ticket_history', type="http", auth='public', website=True)
-    
     def list_tickets(self, **kwargs):
         tickets = request.env["ticket.raise"].sudo().search([])
 
@@ -25,7 +75,7 @@ class TicketController(http.Controller):
             data.append({
                 'ticket_id':ticket.ticket_id,
                 'ticket_type': ticket.ticket_type,
-                'employee': ticket.employee,
+                'product': ticket.product,
                 'priority': ticket.priority,
                 'message': ticket.message,
                 'nepalidatepicker': ticket.nepalidatepicker,
@@ -39,7 +89,7 @@ class TicketController(http.Controller):
     @http.route('/ticket_raise', type='http', auth='public', website=True)
     def authenticate_user(self, **post):
         ticket_type = post.get('ticket_type')
-        employee = post.get('employee')
+        product = post.get('product')
         priority = post.get('priority')
         message = post.get('message')
         client_email= post.get('client_email')
@@ -54,7 +104,7 @@ class TicketController(http.Controller):
 
         ticket = request.env['ticket.raise'].sudo().create({
             'ticket_type': ticket_type,
-            'employee': employee,
+            'product': product,
             'priority':priority,
             'message': message,
             'photo':photo_base64,
@@ -75,5 +125,11 @@ class NewTicketController(http.Controller):
     @http.route('/new_ticket', type="http", auth='public', website=True)
     def user_authentication_form(self, **kw):
         return request.render('support_sys.new_ticket', {})
+    
+class ExpenseController(http.Controller):   
+    @http.route('/expense', type="http", auth='public', website=True)
+    def user_authentication_form(self, **kw):
+        return request.render('support_sys.expense', {})
+    
 
    
--- models/models.py
+++ models/models.py
@@ -1,4 +1,3 @@
-
 from odoo import models, fields, api
 from datetime import datetime
 
@@ -6,103 +5,152 @@ from datetime import datetime
 class support_sys(models.Model):
     _name = 'support_sys.support_sys'
     _description = 'support_sys.support_sys'
-
-
-
-    c_id = fields.Integer(string='ID')
-    name = fields.Char(string='Client Name')
-    image = fields.Binary("Client Image")
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    c_id = fields.Integer(string='ID', tracking=True)
+    name = fields.Char(string='Client Name', tracking=True)
+    image = fields.Binary("Client Image", tracking=True)
     # message = fields.Text(string='Message', required=True)
-    issue_description = fields.Char(string='Issued Description')
+    issue_description = fields.Char(string='Issued Description', tracking=True)
     support_type = fields.Selection([
         ('ordinary', 'Ordinary'),
         ('vip', 'VIP')
-    ], string='Support Type', default='ordinary', help='Select the support type')
-    assigned_employee = fields.Char(string='Name of Employee')
+    ], string='Support Type', default='ordinary', help='Select the support type', tracking=True)
+    assigned_employee = fields.Char(string='Name of Employee', tracking=True)
     status = fields.Selection([
         ('new', 'New'),
         ('in_progress', 'In Progress'),
         ('closed', 'Closed'),
-    ], string='Status', default='new')
+    ], string='Status', default='new', tracking=True)
     month_field = fields.Date(
         string='Date',
         widget='Date',
         help='Select a month',
-        default=datetime.now().date()
+        default=datetime.now().date(), tracking=True
     )
+    day_field = fields.Integer(
+        string='Day',
+        compute='_compute_day_month_year',
+        store=True
+    )
+    months_field = fields.Integer(
+        string='Month',
+        compute='_compute_day_month_year',
+        store=True
+    )
+    year_field = fields.Integer(
+        string='Year',
+        compute='_compute_day_month_year',
+        store=True
+    )
+
+    @api.depends('month_field')
+    def _compute_day_month_year(self):
+        for record in self:
+            record.day_field = record.month_field.day if record.month_field else False
+            record.months_field = record.month_field.month if record.month_field else False
+            record.year_field = record.month_field.year if record.month_field else False
+
+
 class Ticket(models.Model):
     _name = 'ticket.raise'
     _description = 'ticket'
-    _inherit='mail.thread'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     # name = fields.Char(string='Name')
     ticket_id = fields.Char(string='Ticket ID')
-    ticket_type = fields.Selection([('complain', 'Complain'), ('feedback', 'Feedback'), ('request', 'Request'), ('query', 'Query')],
-                                   string='Ticket Type')
-    employee = fields.Selection([('DeerWalk', 'DeerWalk'), ('Techrida', 'Techrida'), ('TalentConnect', 'TalentConnect'), ('TukiLogic', 'TukiLogic'), ('EkSolution', 'EkSolution')],
-                                string='Employer Of')
+    ticket_type = fields.Selection(
+        [('complain', 'Complain'), ('feedback', 'Feedback'), ('request', 'Request'), ('query', 'Query')],
+        string='Ticket Type', tracking=True)
+    # employee = fields.Selection([('DeerWalk', 'DeerWalk'), ('Techrida', 'Techrida'), ('TalentConnect', 'TalentConnect'),
+    #                              ('TukiLogic', 'TukiLogic'), ('EkSolution', 'EkSolution')],
+    #                             string='Employer Of',tracking=True)
+    product = fields.Selection([('Internet', 'Internet'), ('Software', 'Software'), ('Website', 'Website')
+                                ],
+                               string='Choose a Product')
     message = fields.Text(string='Message', required=True)
-    priority=fields.Selection([('urgent', 'Urgent'), ('high', 'High'), ('medium', 'Medium'), ('low', 'Low')],
-                                   string='Priority')
-    photo = fields.Binary(string='photo')
-    nepalidatepicker = fields.Date(string='Requested date', required=True)
-    client_email = fields.Char(string='Client Email')
-    
+    priority = fields.Selection([('urgent', 'Urgent'), ('high', 'High'), ('medium', 'Medium'), ('low', 'Low')],
+                                string='Priority', tracking=True)
+    progress = fields.Selection(
+        [('New', 'New'), ('In Progress', 'In Progress'), ('Completed', 'Completed'), ('Cancelled', 'Cancelled')],
+        default='New', String="Progress", required=True, tracking=True)
+    photo = fields.Binary(string='photo', tracking=True)
+    nepalidatepicker = fields.Date(string='Requested date', required=True, tracking=True)
+    client_email = fields.Char(string='Client Email', tracking=True)
+
     # photo_filename = fields.Char(string='photo Filename')
+
+    def action_confirm(self):
+        self.write({'progress': 'In Progress'})
+
+    def action_decline(self):
+        self.write({'progress': 'Cancelled'})
+
+    def action_completed(self):
+        self.write({'progress': 'Completed'})
+
     @api.model
-    def create(self,vals):
-        print("Sequence",vals)
+    def create(self, vals):
+        print("Sequence", vals)
         vals['ticket_id'] = self.env['ir.sequence'].next_by_code("ticket.raise")
-        return super(Ticket,self).create(vals)
-    
+        return super(Ticket, self).create(vals)
 
 
 class Employee(models.Model):
-    _name='employee.details'
-    _description='employee'
+    _name = 'employee.details'
+    _description = 'employee'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    employee_id = fields.Char(string='Sequence Number')
+    employee_name = fields.Char(string='Employee Name', tracking=True)
+    contact = fields.Char(tracking=True)
+    address = fields.Char(tracking=True)
+    department = fields.Char(tracking=True)
+    position = fields.Char(tracking=True)
+    salary = fields.Integer(tracking=True)
+    date_joined = fields.Date(tracking=True)
+    _sql_constraints = [
+        ('employee_id_uniq', 'unique(employee_id)', 'Employee ID must be unique!'),
+    ]
 
-    
-    employee_name=fields.Char()
-    contact= fields.Char()
-    address = fields.Char()
-    department=fields.Char()
-    position = fields.Char()
-    salary=fields.Integer()
-    date_joined=fields.Date()
+    @api.model
+    def create(self, vals):
+        print("Sequence", vals)
+        vals['employee_id'] = self.env['ir.sequence'].next_by_code("employee.details")
+        return super(Employee, self).create(vals)
 
-
-   
-    
     class Expense(models.Model):
-        _name='employee.expense'
-        _description='employee'
-
-        expense_date = fields.Date(string='Expense Date', default=fields.Date.today())
-        amount = fields.Float(string='Amount')
+        _name = 'employee.expense'
+        _description = 'employee'
+        _inherit = ['mail.thread', 'mail.activity.mixin']
+        nepalidatepicker = fields.Date(string='Expense Date', default=fields.Date.today())
+        amount = fields.Float(string='Amount', default=None)
         description = fields.Text(string='Description')
-        status = fields.Selection([
-            ('draft', 'Draft'),
-            ('submitted', 'Submitted'),
-            ('approved', 'Approved'),
-            ('rejected', 'Rejected'),
-        ], string='Status', default='draft')
+        # status = fields.Selection([
+        #     ('draft', 'Draft'),
+        #     ('submitted', 'Submitted'),
+        #     ('approved', 'Approved'),
+        #     ('rejected', 'Rejected'),
+        # ], string='Status', default='draft',tracking=True)
         expense_category = fields.Selection([
-            ('meals', 'Meals'),
-            ('travel', 'Travel'),
-            ('accommodation', 'Accommodation'),
-            ('other', 'Other'),
-        ], string='Expense Category')
+            ('Meals', 'Meals'),
+            ('Travel', 'Travel'),
+            ('Accomodation', 'Accomodation'),
+            ('Other', 'Other'),
+        ], string='Expense Category', tracking=True)
+        status = fields.Selection(
+            [('New', 'New'), ('Accepted', 'Accepted'), ('Rejected', 'Rejected')],
+            default='New', String="Progress", required=True, tracking=True)
+        starting_location = fields.Char(string='Starting Location', tracking=True)
+        ending_location = fields.Char(string='Ending Location', tracking=True)
 
-        starting_location = fields.Char(string='Starting Location')
-        ending_location = fields.Char(string='Ending Location')
-        bill_image = fields.Binary("Bill Image")
+        photo = fields.Binary("Bill Image", tracking=True)
+        hotel_name = fields.Char("Hotel Name")
+        hotel_address = fields.Char("Address",tracking=True)
+        hotel_phone = fields.Char("Phone", tracking=True) 
+        def action_accepted(self):
+            self.write({'status': 'Accepted'})
 
-    
+        def action_rejected(self):
+            self.write({'status': 'Rejected'})
 
     def submit_ticket(self):
-        # Add logic to handle ticket submission
-        # You can process the form data and perform any necessary actions here
         return {'type': 'ir.actions.act_window_close'}
-    
-
-    
